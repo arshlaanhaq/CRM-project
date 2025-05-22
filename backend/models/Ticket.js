5513e684a6cfde6fdc1d2858e2aa04799df77e00
Arshlaan haque <arshlaanhaq005@gmail.com>
2025-05-23 01:28:56 +0530

    new changes

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
+  priority: { type: String, enum: ['low', 'medium', 'high','urgent'], default: 'medium' },
   status: { type: String, enum: ['open', 'in-progress', 'closed', 'resolved'], default: 'open' },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
