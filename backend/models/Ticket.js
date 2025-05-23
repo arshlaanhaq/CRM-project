6d3908a7034b934e3a673cd1bc055c3ae88fdb07
harshkanaujia28 <154357957+harshkanaujia28@users.noreply.github.com>
2025-05-23 12:03:45 +0530

    fix

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-<<<<<<< HEAD
   priority: {
     type: String,
     enum: ["low", "medium", "high", "urgent"],
@@ -25,12 +24,12 @@ const ticketSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
-=======
+
   priority: { type: String, enum: ['low', 'medium', 'high','urgent'], default: 'medium' },
   status: { type: String, enum: ['open', 'in-progress', 'closed', 'resolved'], default: 'open' },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
->>>>>>> 5513e684a6cfde6fdc1d2858e2aa04799df77e00
+
 
   customer: {
     name: { type: String, required: true },
