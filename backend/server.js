29886c87a439850f59f636055681703b2cf70103
Arshlaan haque <arshlaanhaq005@gmail.com>
2025-06-12 18:38:40 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,16 @@ app.use(cors({
   credentials: true,              // important for cookies/auth headers
 }));
 app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://82.25.109.100:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
